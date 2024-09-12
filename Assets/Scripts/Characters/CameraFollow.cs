564595f930fb41902ba613765bf1fd59e2e13be1
Dmitrii Zolotuhin <dmitriizolotuhin@cloud.upwork.com>
2024-09-12 16:19:37 +0300

    Added some logic separation

--- Assets/Scripts/Characters/CameraFollow.cs
+++ Assets/Scripts/Characters/CameraFollow.cs
@@ -1,50 +1,28 @@
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour
-{
-    public Transform target; // Ссылка на персонажа, за которым нужно следить
-    public Vector3 offset; // Смещение камеры от персонажа
-    public float speed = 0.01f; // Скорость премещения камеры
-    private const float FixedZ = -10; // Фиксированное значение Z для камеры
+public class CameraFollow : MonoBehaviour {
+  public Transform target; // Ссылка на персонажа, за которым нужно следить
+  public Vector3 offset; // Смещение камеры от персонажа
+  public float speed = 0.01f; // Скорость премещения камеры
+  private const float FixedZ = -10; // Фиксированное значение Z для камеры
 
-    public float checkInterval = 0.10f; // Интервал проверки на наличие персонажа для слежки
-    private bool _targetFound;
-    
-    void Start()
-    {
-        StartCoroutine(FindTarget());
+  private void Start() {
+    var player = GameObject.FindWithTag("Player");
+    if (player == null) {
+      throw new System.Exception("Player not found");
     }
 
-    private System.Collections.IEnumerator FindTarget()
-    {
-        while (!_targetFound) 
-        {
-            GameObject player = GameObject.FindWithTag("Player");
+    target = player.transform;
+  }
 
-            if (player != null)
-            {
-                target = player.transform;
-                _targetFound = true;
-                Debug.Log("Player found, camera is now following.");
-            }
-            else
-            {
-                Debug.Log("Player not found, trying again...");
-            }
-            
-            yield return new WaitForSeconds(checkInterval);
-        }
-    }
 
-    private void LateUpdate()
-    {
-        if (target != null)
-        {
-            Vector3 desiredPosition = target.position + offset;
-            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, speed);
-            smoothedPosition.z = FixedZ;
-            
-            transform.position = smoothedPosition;
-        }
-    }
+  private void LateUpdate() {
+    if (target == null) return;
+
+    var desiredPosition = target.position + offset;
+    var smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, speed);
+    smoothedPosition.z = FixedZ;
+
+    transform.position = smoothedPosition;
+  }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,89 +1,26 @@
-using System.Collections;
+using System;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
-{
-   public PlayerMovement player; //Ссылка на скрипт игрока
-   public EnemyMovement enemy; // Ссылка на скрипт врага
+public class GameManager : MonoBehaviour {
+  public Movement player; //Ссылка на скрипт игрока
+  public EnemyMovement enemy; // Ссылка на скрипт врага
 
-   public float checkInterval = 0.05f; // Интервал между попытками поиска
-   private bool _playerFound;
-   private bool _enemyFound;
-   public Vector3Int targetTile;
+  private Camera _mainCamera;
+  private GameState _gameState = GameState.PlayerTurn;
+  public static event Action<GameState> OnGameStateChanged;
 
-   void Start()
-   {
-      StartCoroutine(FindPlayerAndEnemy());
-   }
 
-   private IEnumerator FindPlayerAndEnemy()
-   {
-      while (!_playerFound || !_enemyFound) 
-      {
-         if (!_playerFound)
-         {
-            PlayerMovement foundPlayer = FindObjectOfType<PlayerMovement>();
+  private void Awake() {
+    var spawner = FindObjectOfType<SpawnManager>();
+    spawner.SpawnPlayer();
+    _mainCamera = Camera.main;
+  }
+}
 
-            if (foundPlayer != null)
-            {
-               player = foundPlayer;
-               _playerFound = true;
-               Debug.Log("Player found.");
-            }
-            else
-            {
-               Debug.Log("Player not found, trying again...");
-            }
-         }
-         
-         if (!_enemyFound)
-         {
-            EnemyMovement foundEnemy = FindObjectOfType<EnemyMovement>();
-
-            if (foundEnemy != null)
-            {
-               enemy = foundEnemy;
-               _enemyFound = true;
-               Debug.Log("Enemy found.");
-            }
-            else
-            {
-               Debug.Log("Enemy not found, trying again...");
-            }
-         }
-         
-         yield return new WaitForSeconds(checkInterval);
-      }
-
-      Debug.Log("Both Player and Enemy found, game can proceed.");
-   }
-   private void Update()
-   {
-      if (Input.GetMouseButtonDown(0))
-      {
-         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-         mousePos.z = 0f;
-
-         targetTile = player.tilemap.WorldToCell(mousePos);
-
-         // Если игрок кликнет на себя, то ничего не произойдет
-         if (targetTile == player.currentTile)
-         {
-            Debug.Log("Player is already on this tile.");
-            return;
-         }
-         
-         if (player.IsWithinOneTileRadius(targetTile) && player.TileExists(targetTile))
-         {
-            player.MoveToTile(targetTile);
-
-            enemy.MoveTowardsPlayer();
-         }
-         
-         else
-         {
-            Debug.Log("Cannot move player to the target tile.");
-         }
-      }
-   }
+public enum GameState {
+  PlayerTurn,
+  EnemyTurn,
+  EnvironmentTurn,
+  SpawningPhase,
+  GameOver
 }
