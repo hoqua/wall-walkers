da8147d61a853411b9b0529dbf52a8963a3f3250
Scribbletalk <82647295+Scribbletalk@users.noreply.github.com>
2024-09-26 15:00:54 +0300

    Attack animation (#12)
    * Добавлена анимация атаки для игрока.
    
    * Недостающий коммит
    
    * Враг теперь не может атаковать после смерти. Убрал ненужный Log
    
    * Добавил анимацию эффекта удара (slash) для игрока.
    
    * Сделал анимацию слеш эффекта для врага.

--- Assets/Scripts/Enemy/EnemyAttack.cs
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class EnemyAttack : MonoBehaviour
 {
-   public int damage = 1;      // Сколько урона наносит враг
-   public int attackRange = 1; // Радиус атаки (1 = одна клетка)
+   public int damage = 1;                 // Сколько урона наносит враг
+   public int attackRange = 1;            // Радиус атаки (1 = одна клетка)
+   
+   public GameObject slashObject;         // Префаб для эффекта удара
+   private readonly float _slashDuration = 0.5f; // Длительность эффекта атаки (slash)
    
    private PlayerStats _player;
-
+   private Transform _enemy;
+   
    void Start()
    {
+      _enemy = GetComponent<Transform>();
       _player = FindObjectOfType<PlayerStats>();
+      slashObject.SetActive(false);
    }
-
+   
    // Проверяет есть ли в радиусе врага игрок
    public bool IsPlayerInRange(Vector3Int enemyTile, Vector3Int playerTile)
    {
@@ -24,11 +30,31 @@ public class EnemyAttack : MonoBehaviour
    
    public void AttackPlayer()
    {
-      if (_player != null)
+      if (_player != null && _enemy != null)
       {
          _player.TakeDamage(damage);
          Debug.Log($"Enemy attacks Player for {damage} damage.");
+
+         Vector3 attackDirection = (_player.transform.position - _enemy.position).normalized;
+         
+         ShowSlashEffect(attackDirection);
       }
    }
 
+   // Слеш эффект
+   private void ShowSlashEffect(Vector3 attackDirection)
+   {
+      slashObject.SetActive(true);
+
+      float angle = Mathf.Atan2(attackDirection.y, attackDirection.x) * Mathf.Rad2Deg;
+      slashObject.transform.rotation = Quaternion.Euler(0, 0, angle - 40);
+      
+      Invoke(nameof(DeactivateSlash), _slashDuration);
+   }
+
+   // Деактивация слеш эффекта
+   private void DeactivateSlash()
+   {
+      slashObject.SetActive(false);  
+   }
 }
--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -1,26 +1,88 @@
-
 using UnityEngine;
-
 
 public class PlayerAttack : MonoBehaviour
 {
-   public int damage = 1;           // Урон игрока
-   public EnemyStats targetEnemy;   // Враг которого игрок будет атаковать
-   
-   public void Attack(GameObject enemy)
-   {
-       var enemyStats = enemy.GetComponent<EnemyStats>();
-       targetEnemy = enemyStats;
-       targetEnemy.TakeDamage(damage);
-   }
+    public Animator animator;                      // Аниматор и значения для анимаций
+    public GameObject slashObject;                 // Префаб для эффекта удара
+    private readonly float _attackDuration = 0.7f; // Длительность анимации удара
+    private readonly float _slashDuration = 0.5f;  // Длительность анимации эффекта удара (slash)
+    
+    private static readonly int Horizontal = Animator.StringToHash("Horizontal");
+    private static readonly int Vertical = Animator.StringToHash("Vertical");
+    private static readonly int AttackTrigger = Animator.StringToHash("AttackTrigger");
+    private static readonly int ReturnToIdleTrigger = Animator.StringToHash("ReturnToIdleTrigger"); 
+    
+    public int damage = 1;               // Урон игрока
+    private Transform _playerTransform;  // Положение игрока
+    public EnemyStats targetEnemy;       // Враг которого игрок будет атаковать
+    
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        _playerTransform = transform;
+        slashObject.SetActive(false);
+    }
+    
+    public void Attack(GameObject enemy)
+    {
+        // Направление атаки
+        var direction = (enemy.transform.position - _playerTransform.position).normalized;
 
-   public bool CheckIfWillKillEnemy(GameObject enemy)
-   {
-       var enemyStats = enemy.GetComponent<EnemyStats>();
-       if (enemyStats != null)
-       {
-           return enemyStats.health - damage <= 0;
-       }
-       return false;
-   }
+        // Задаем направление атаки в аниматоре
+        SetAttackDirectionInAnimator(direction);
+        
+        animator.SetTrigger(AttackTrigger);
+        
+        // Эффект удара
+        ShowSlashEffect(direction);
+        
+        // Нанесение урона
+        var enemyStats = enemy.GetComponent<EnemyStats>();
+        targetEnemy = enemyStats;
+        targetEnemy.TakeDamage(damage);
+        
+        // Переход в Idle после атаки
+        Invoke(nameof(ReturnToIdle), _attackDuration);
+    }
+
+    // Слеш эффект
+    private void ShowSlashEffect(Vector3 direction)
+    {
+        slashObject.SetActive(true);
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        slashObject.transform.rotation = Quaternion.Euler(0, 0, angle - 40);
+        
+        Invoke(nameof(DeactivateSlash), _slashDuration);
+    }
+
+    // Деактивация слеш эффекта
+    private void DeactivateSlash()
+    {
+        slashObject.SetActive(false);
+    }
+
+    private void SetAttackDirectionInAnimator(Vector3 direction)
+    {
+        float horizontal = Mathf.Round(direction.x);
+        float vertical = Mathf.Round(direction.y);
+        
+        animator.SetFloat(Horizontal, horizontal);
+        animator.SetFloat(Vertical, vertical);
+    }
+    
+    // Возвращает анимацию в исходное положение после атаки
+    private void ReturnToIdle()
+    {
+        animator.SetTrigger(ReturnToIdleTrigger);
+        animator.SetFloat(Horizontal, 0);
+        animator.SetFloat(Vertical, -1);
+    }
+
+    public bool CheckIfWillKillEnemy(GameObject enemy)
+    {
+        var enemyStats = enemy.GetComponent<EnemyStats>();
+        if (enemyStats != null) return enemyStats.health - damage <= 0;
+        return false;
+    }
 }
