e8301e442f57fa99d2464ca4748e93da75c51c01
Bocchi the Code! <catreen02@gmail.com>
2025-02-27 16:52:12 +0200

    Добавлен эффект атаки для мимика

--- Assets/Scripts/Enemies/Mimic/Mimic.cs
+++ Assets/Scripts/Enemies/Mimic/Mimic.cs
@@ -8,12 +8,14 @@ namespace Enemies.Mimic
         private EnemyStats _enemyStats;
         private PlayerMovement _player;
         private PlayerStats _playerStats;
+        
+        [SerializeField] private GameObject attackEffectPrefab; // Префаб эффекта атаки
+        [SerializeField] private float attackEffectDuration = 1f; // Время жизни эффекта
 
         private void Awake()
         {
             _gameManager = FindObjectOfType<GameManager>();
             _gameManager.AddEnemy(this);
-            Debug.Log("Мимик создан!"); // Проверяем, создается ли мимик
         }
 
         public override void EnemyTurn()
@@ -24,21 +26,38 @@ namespace Enemies.Mimic
                 if (_playerStats == null) _playerStats = FindObjectOfType<PlayerStats>();
                 if (_enemyStats == null) _enemyStats = GetComponent<EnemyStats>();
 
-
-                float distance = Vector3.Distance(transform.position, _player.transform.position);
-                Debug.Log("Расстояние до игрока: " + distance);
-
-                if (distance <= _enemyStats.attackRange)
+                if (_player != null)
                 {
-                    AttackPlayer();
+                    float distance = Vector3.Distance(transform.position, _player.transform.position);
+
+                    if (distance <= _enemyStats.attackRange)
+                    {
+                        AttackPlayer();
+                    }
                 }
             }
         }
 
         private void AttackPlayer()
         {
-            Debug.Log("Мимик атакует игрока!");
             _playerStats.TakeDamage(_enemyStats.damage);
+            ShowAttackEffect();
+        }
+        
+        private void ShowAttackEffect()
+        {
+            if (attackEffectPrefab != null)
+            {
+                var playerPosition = _player.transform.position;
+                playerPosition.y = transform.position.y + 0.015f;   //Корректировка высоты отображения эффекта
+                GameObject effect = Instantiate(attackEffectPrefab, playerPosition, Quaternion.identity);
+                effect.SetActive(true);
+                Destroy(effect, attackEffectDuration);
+            }
+            else
+            {
+                Debug.LogError("Ошибка: Префаб эффекта атаки не установлен в мимике!");
+            }
         }
     }
 }
